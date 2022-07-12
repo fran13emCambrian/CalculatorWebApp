f3b64c743e0c9de81cfe1fba4a48ed3fca92e776
Francisco <a00223609@mycambrian.ca>
2022-07-11 23:38:18 -0230

    #Add Controller

--- WebAPI/Controllers/CalculatorController.cs
+++ WebAPI/Controllers/CalculatorController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CalculatorLogic;
+
 
 namespace WebAPI.Controllers
 {
@@ -7,5 +12,12 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        //Takes numbers for addition
+        [HttpGet]
+        public double Add([FromQuery] double num1, [FromQuery] double num2)
+        {
+            return Class1.Addition(num1, num2);
+        }
+
     }
 }
